ae57b8ebe823690200b55225a09ab3b82de9a27f
rocketxx <william97.ragusa@gmail.com>
2023-06-23 16:34:55 +0200

    s

--- screens/WelcomeAdminScreen.js
+++ screens/WelcomeAdminScreen.js
@@ -2,39 +2,41 @@
 import { useEffect, useState } from 'react';
 import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
 import { firebase } from '../Firebase/firebase';
-import jwt_decode from "jwt-decode";
 function WelcomeAdminScreen() {
-//   const todo = firebase.firestore().collection('courses').doc("64RqlG8KhjK8GYuTWwjQ____");
+    firebase.auth().signInWithEmailAndPassword("cane@gmail.com", "william")
+    // firebase.auth().createUserWithEmailAndPassword("testmio@gmail.com","william");
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user) {
+            var uid = user.uid;
+            console.log(uid)
+            user.getIdTokenResult().then((idTokenResult) => {
+                const customClaims = idTokenResult.claims;
+                console.log(idTokenResult.token)
+                // console.log("CLAIMS: ", customClaims["admin"])
+            });
+        } else {
+            console.log("niente")
+        }
+    });
 
-////lettura
-//   todo.get().then((doc) => {
-//     if (doc.exists) {
-//         console.log("Document data:", doc.data());
-//     } else {
-//         console.log("No such document!");
-//     }
-// }).catch((error) => {
-//     console.log("Error getting document:", error);
-// });
+    //scrittura
+    // firebase.auth().setc
+    // const todo2 = firebase.firestore().collection('todos').doc("LWmqBEQ7kGOLNdDK3SuR").set({
+    //   name: "Los Angeles",
+    //   state: "CA",
+    //   country: "USA"
+    // })
+    // .then(() => {
+    //   console.log("Document successfully written!");
+    // })
+    // .catch((error) => {
+    //   console.error("Error writing document: ", error);
+    // });
+    //
 
-//scrittura
-// firebase.auth().setc
-// const todo2 = firebase.firestore().collection('todos').doc("LWmqBEQ7kGOLNdDK3SuR").set({
-//   name: "Los Angeles",
-//   state: "CA",
-//   country: "USA"
-// })
-// .then(() => {
-//   console.log("Document successfully written!");
-// })
-// .catch((error) => {
-//   console.error("Error writing document: ", error);
-// });
-//
-
-  return (
-    <View style={styles.rootContainer}>
-      {/* <FlatList
+    return (
+        <View style={styles.rootContainer}>
+            {/* <FlatList
         data={users}
         numColumns={1}
         renderItem={({item}) =>(
@@ -45,24 +47,24 @@ function WelcomeAdminScreen() {
           </Pressable>
         )}
       ></FlatList> */}
-      <Text style={styles.title}>Benvenuto!</Text>
-      <Text>ADMIN!</Text>
-    </View>
-  );
+            <Text style={styles.title}>Benvenuto!</Text>
+            <Text>ADMIN!</Text>
+        </View>
+    );
 }
 
 export default WelcomeAdminScreen;
 
 const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 32,
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    marginBottom: 8,
-  },
+    rootContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 32,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 8,
+    },
 });
