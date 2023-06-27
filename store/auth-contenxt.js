1d596633f861f60fddc502de1355c3df083294c5
rocketxx <william97.ragusa@gmail.com>
2023-06-27 22:41:24 +0200

    store auth

--- store/auth-contenxt.js
+++ store/auth-contenxt.js
@@ -1,39 +1,59 @@
 import { createContext, useState } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { FIREBASE_AUTH } from "../Firebase/firebase";
 export const AuthContext = createContext({
-    token: '',
+    user: '',
     isAuthenticated: false,
     isAdmin : false,
     setAdmin : () => { }, 
-    authenticate: (token) => { },
+    authenticate: (user,adminClaims) => { },
     logout: () => { },
 });
 
 function AuthContextProvider({ children }) {
-    const [authToken, setAuthToken] = useState();
-    const [isAdmin, setIsAdmin] = useState();
+    const [authUser, setUser] = useState(null);
+    const [isAdmin, setIsAdmin] = useState(false);
 
-    function authenticate(token) {
-        // setAuthToken(token);
-        // console.log(token)
-        // AsyncStorage.setItem('token', token); //firebase lo fa scadere dopo 1h, attento
+    async function authenticate(user,adminClaims) {
+        setUser(JSON.stringify(user));
+        console.log("USER: ",user);
+        await AsyncStorage.setItem('userLogged',JSON.stringify(user)); //firebase lo fa scadere dopo 1h, attento
+        if(adminClaims)
+        {
+            await AsyncStorage.setItem('isAdmin','true');
+            setAdmin(true)
+
+        }
+        else
+        {
+            await AsyncStorage.setItem('isAdmin','false');
+            setAdmin(false)
+        }
+        // var mytmp = tmp.claims
+        // console.log("CONTEXT_CLAIMS: ",mytmp)
     }
 
     function logout() {
-        // setAuthToken(null);
-        // setIsAdmin(false)
-        // AsyncStorage.removeItem('token');
+        setAuthToken(null);
+        setIsAdmin(false)
+        AsyncStorage.removeItem('userLogged');
+        AsyncStorage.removeItem('isAdmin');
     }
 
-    function setAdmin()
+    async function setAdmin()
     {
         setIsAdmin(!isAdmin)
+        if(isAdmin)
+        await AsyncStorage.setItem('isAdmin','true');
+        else
+        await AsyncStorage.setItem('isAdmin','false');
+        console.log("ADMIN: ",isAdmin)
     }
 
     const value = {
-        token: authToken,
-        isAuthenticated: !!authToken, //!!nome_var converte il valore in booleano. true se è pieno, altrim. se vuoto, stringaa vuota o null false
-        isAdmin : !!isAdmin,
+        user: authUser,
+        isAuthenticated: !!authUser, //!!nome_var converte il valore in booleano. true se è pieno, altrim. se vuoto, stringaa vuota o null false
+        isAdmin : isAdmin,
         authenticate: authenticate,
         logout: logout,
         setAdmin : setAdmin
